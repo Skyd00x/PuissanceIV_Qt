0be5e27956a93374653a5cca84c48222f647b9dd
Andrea <159561917+Skyd00x@users.noreply.github.com>
2026-01-07 18:17:55 +0100

    Correction seuil de confiance

--- CameraAi.cpp
+++ CameraAi.cpp
@@ -195,7 +195,7 @@ std::vector<Detection> CameraAI::inferTorch(const cv::Mat& frameBGR)
     at::Tensor conf, labels;
     std::tie(conf, labels) = cls_scores.max(1);
 
-    const float confTh = 0.1f;  // Filtrer les prédictions
+    const float confTh = 0.5f;  // Filtrer les prédictions
     at::Tensor keep = conf > confTh;
     boxes_xywh = boxes_xywh.index({keep});
     conf = conf.index({keep});
